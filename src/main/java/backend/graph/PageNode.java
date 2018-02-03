4b7a8e2a84dcfc096b3f617f4aeedc32b4fbded4
Xiaoming <mail@xiaoming.se>
2018-02-03 12:51:26 +0100

    Add PageNode helper class for network graphNode

---
+++ src/main/java/backend/graph/PageNode.java
@@ -0,0 +1,28 @@
+package backend.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import fastily.jwiki.core.Wiki;
+
+public class PageNode {
+    private Wiki wiki = new Wiki("en.wikipedia.org");
+    private final String pageURL;
+    private final List<String> neighbourPage = new ArrayList();
+
+    public PageNode(String pageURL){
+        this.pageURL = pageURL;
+    }
+
+    public List<String> getNeighbours(){
+        if(this.neighbourPage.isEmpty()){
+            neighbourPage.addAll(findNeighbours());
+        }
+        return this.neighbourPage;
+    }
+
+    private List<String> findNeighbours() {
+        List<String> neighboursFound = wiki.getLinksOnPage(this.pageURL);
+        return neighboursFound;
+    }
+
+}
